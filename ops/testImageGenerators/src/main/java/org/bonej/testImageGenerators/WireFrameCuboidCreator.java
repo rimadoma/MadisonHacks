c53defb18c4769874b466c61013417366113b1fe
rimadoma <rdomander@rvc.ac.uk>
2016-02-22 13:59:06 -0600

    Project files

---
+++ ops/testImageGenerators/src/main/java/org/bonej/testImageGenerators/WireFrameCuboidCreator.java
@@ -0,0 +1,24 @@
+package org.bonej.testImageGenerators;
+
+import net.imagej.ops.Op;
+import net.imagej.ops.OpEnvironment;
+
+/**
+ * @author Richard Domander
+ */
+public class WireFrameCuboidCreator implements Op {
+    @Override
+    public OpEnvironment ops() {
+        return null;
+    }
+
+    @Override
+    public void setEnvironment(OpEnvironment opEnvironment) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
