a02934044e8b131deb2616d1bd979096c5de5e9b
rimadoma <rdomander@rvc.ac.uk>
2016-03-08 10:36:41 -0600

    Add convenience methods

--- utilities/src/main/java/org/bonej/utilities/CalibratedAxisUtil.java
+++ utilities/src/main/java/org/bonej/utilities/CalibratedAxisUtil.java
@@ -3,7 +3,9 @@ package org.bonej.utilities;
 import net.imagej.axis.CalibratedAxis;
 import net.imagej.space.AnnotatedSpace;
 
-import java.util.Arrays;
+import javax.annotation.Nullable;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -11,7 +13,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * Utilities for dealing with CalibratedAxis
  *
  * @author Richard Domander
- * @todo Can an AnnotatedSpace have zero dims? What happens then?
  */
 public final class CalibratedAxisUtil {
     private CalibratedAxisUtil() {} // There's no reason to create an instance of this class
@@ -25,22 +26,47 @@ public final class CalibratedAxisUtil {
             throws NullPointerException {
         checkNotNull(space, "Cannot determine dimensions of a null space");
 
+        final Stream<CalibratedAxis> axes = getAxisStream(space);
+        return axes.filter(a -> a.type().isSpatial()).count();
+    }
+
+    /**
+     * Returns the CalibratedAxis in the given space as a Stream,
+     * or an empty Stream if the space is null
+     */
+    public static <T extends AnnotatedSpace<CalibratedAxis>> Stream<CalibratedAxis> getAxisStream(
+            @Nullable final T space) {
+        final Optional<CalibratedAxis[]> axes = allocateAndGetAxis(space);
+        return axes.isPresent() ? Stream.of(axes.get()) : Stream.empty();
+    }
+
+    /**
+     * Allocates and returns the CalibratedAxis in the given space as an Optional of array,
+     * or an empty Optional if the space is null
+     *
+     * @todo Can an AnnotatedSpace have zero dims?
+     */
+    public static <T extends AnnotatedSpace<CalibratedAxis>> Optional<CalibratedAxis[]> allocateAndGetAxis(
+            @Nullable final T space) {
+        if (space == null) {
+            return Optional.empty();
+        }
+
         final CalibratedAxis[] axes = new CalibratedAxis[space.numDimensions()];
         space.axes(axes);
-        return Arrays.stream(axes).filter(a -> a.type().isSpatial()).count();
+        return Optional.of(axes);
     }
 
     /**
      * Returns true if the given space has dimensions that are not spatial, e.g. time or channel
      *
-     * @throws NullPointerException
+     * @throws NullPointerException if space == null
      */
     public static <T extends AnnotatedSpace<CalibratedAxis>> boolean hasNonSpatialDimensions(final T space)
             throws NullPointerException {
         checkNotNull(space, "Cannot determine dimensions of a null space");
 
-        final CalibratedAxis[] axes = new CalibratedAxis[space.numDimensions()];
-        space.axes(axes);
-        return Arrays.stream(axes).anyMatch(a -> !a.type().isSpatial());
+        final Stream<CalibratedAxis> axes = getAxisStream(space);
+        return axes.anyMatch(a -> !a.type().isSpatial());
     }
 }
--- utilities/src/test/java/org/bonej/utilities/CalibratedAxisUtilTest.java
+++ utilities/src/test/java/org/bonej/utilities/CalibratedAxisUtilTest.java
@@ -1,12 +1,11 @@
 package org.bonej.utilities;
 
 
-import net.imagej.Dataset;
-import net.imagej.DatasetService;
 import net.imagej.ImageJ;
 import net.imagej.ImgPlus;
 import net.imagej.axis.Axes;
 import net.imagej.axis.AxisType;
+import net.imagej.axis.CalibratedAxis;
 import net.imagej.ops.Ops;
 import net.imagej.ops.special.function.BinaryFunctionOp;
 import net.imagej.ops.special.function.Functions;
@@ -14,40 +13,41 @@ import net.imglib2.Dimensions;
 import net.imglib2.FinalDimensions;
 import net.imglib2.img.Img;
 import net.imglib2.type.logic.BitType;
-import org.apache.commons.math.exception.NullArgumentException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit tests for the CalibratedAxisUtil class
  *
  * @author Richard Domander
- * @todo Use DatasetCreator
  */
 public class CalibratedAxisUtilTest {
     private static final ImageJ IMAGE_J = new ImageJ();
+    private static final FinalDimensions DIMENSIONS = new FinalDimensions(10, 10, 10);
+    private static final int EXPECTED_DIMENSIONS = DIMENSIONS.numDimensions();
     private static BinaryFunctionOp<Dimensions, BitType, Img<BitType>> imgCreator;
+    private static ImgPlus<BitType> testImgPlus3D;
 
     @BeforeClass
     public static void oneTimeSetUp() {
         imgCreator = (BinaryFunctionOp) Functions
                 .binary(IMAGE_J.op(), Ops.Create.Img.class, Img.class, Dimensions.class, new BitType());
+        final Img<BitType> img = imgCreator.compute1(DIMENSIONS);
+        testImgPlus3D = new ImgPlus<>(img, "", new AxisType[]{Axes.X, Axes.Y, Axes.Z});
     }
 
     @Test
     public void testCountSpatialDimensions() throws AssertionError {
-        final FinalDimensions dimensions = new FinalDimensions(10, 10, 10);
-        final int expectedDimensions = dimensions.numDimensions();
-        final Img<BitType> img = imgCreator.compute1(dimensions);
-        // If you call the constructor with a single argument,
-        // the axis types by default will be Axes.X, Axes.Y and *unknown*
-        final ImgPlus<BitType> imgPlus = new ImgPlus<>(img, "", new AxisType[]{Axes.X, Axes.Y, Axes.Z});
+        final long result = CalibratedAxisUtil.countSpatialDimensions(testImgPlus3D);
 
-        final long result = CalibratedAxisUtil.countSpatialDimensions(imgPlus);
-
-        assertEquals("Wrong number of spatial dimensions counted", expectedDimensions, result);
+        assertEquals("Wrong number of spatial DIMENSIONS counted", EXPECTED_DIMENSIONS, result);
     }
 
     @Test(expected = NullPointerException.class)
@@ -63,22 +63,46 @@ public class CalibratedAxisUtilTest {
 
         final boolean result = CalibratedAxisUtil.hasNonSpatialDimensions(imgPlus);
 
-        assertTrue("Image should contain non-spatial dimensions", result);
+        assertTrue("Image should contain non-spatial DIMENSIONS", result);
     }
 
     @Test
     public void testHasNonSpatialDimensionsReturnsFalseWithOnlySpatialDimensions() throws AssertionError {
-        final FinalDimensions dimensions = new FinalDimensions(10, 10);
-        final Img<BitType> img = imgCreator.compute1(dimensions);
-        final ImgPlus<BitType> imgPlus = new ImgPlus<>(img, "", new AxisType[]{Axes.X, Axes.Y});
+        final boolean result = CalibratedAxisUtil.hasNonSpatialDimensions(testImgPlus3D);
 
-        final boolean result = CalibratedAxisUtil.hasNonSpatialDimensions(imgPlus);
-
-        assertFalse("Image should contain only spatial dimensions", result);
+        assertFalse("Image should contain only spatial DIMENSIONS", result);
     }
 
     @Test(expected = NullPointerException.class)
     public void testHasNonSpatialDimensionsThrowsNullPointerExceptionIfSpaceIsNull() throws AssertionError {
         CalibratedAxisUtil.hasNonSpatialDimensions(null);
+    }
+
+    @Test
+    public void testAllocateAndGetAxis() throws AssertionError {
+        final Optional<CalibratedAxis[]> result = CalibratedAxisUtil.allocateAndGetAxis(testImgPlus3D);
+        assertTrue("Array should be present", result.isPresent());
+        assertEquals("Array has wrong length", EXPECTED_DIMENSIONS, result.get().length);
+    }
+
+    @Test
+    public void testAllocateAndGetAxisReturnsEmptyIfSpaceIsNull() {
+        final Optional<CalibratedAxis[]> result = CalibratedAxisUtil.allocateAndGetAxis(null);
+
+        assertFalse("Optional should be empty", result.isPresent());
+    }
+
+    @Test
+    public void testGetAxisStream() throws AssertionError {
+        final Stream<CalibratedAxis> result = CalibratedAxisUtil.getAxisStream(testImgPlus3D);
+
+        assertEquals("Stream has wrong number of elements", EXPECTED_DIMENSIONS, result.count());
+    }
+
+    @Test
+    public void testGetAxisStreamReturnsEmptyIfSpaceIsNull() {
+        final Stream<CalibratedAxis> result = CalibratedAxisUtil.getAxisStream(null);
+
+        assertFalse(result.findAny().isPresent());
     }
 }
